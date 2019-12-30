6c0d96bdab6dc1fea52ec760d9e36b67f5d6a72b
杜浩然 <903608442@qq.com>
2019-12-30 12:00:41 +0800

    kmp complete

--- kmp.php
+++ kmp.php
@@ -1,29 +1,55 @@
 <?php
 function kmpCheck($str, $search_str) {
-    $str_length = count($str);
-    $search_str_length = count($search_str);
+    $str_length = strlen($str);
+    $search_str_length = strlen($search_str);
     if (empty($str) || empty($search_str) || ($search_str > $str_length)) {
         return false;
     }
     $next = [];
+    $next[0] = -1;
     $search_str_place = 0;
+    $str_place = 0;
     $complete = false;
     $match = false;
-    do {
-        if (($search_str_place + $search_str_length) > $str_length) {
-            break;
+    $k = 0;
+    $l = -1;
+    while ($k < $search_str_length - 1) {
+        if ($l == -1 || $search_str[$k] == $search_str[$l]) {
+            $k++;
+            $l++;
+            $next[$k] = $l;
+        } else {
+            $l = $next[$l];
         }
-        for ($i = 0,$j = $search_str_place; $i <= ($search_str_length - 1); $i++,$j++) {
-            if($search_str[$i] == $str[$j]){
-                continue ;
-            }else{
-
+    }
+    $count = 0;
+    do {
+        $count++;
+        if ($str[$str_place] == $search_str[$search_str_place]) {
+            $str_place++;
+            $search_str_place++;
+        } else {
+            if ($next[$search_str_place] == -1) {
+                $str_place++;
+                $search_str_place = 0;
+            } else {
+                $search_str_place = $next[$search_str_place];
             }
         }
-    } while ($complete == false || $match == false);
+        if ($str_place >= ($str_length)) {
+            $complete = true;
+        }
+        if ($search_str_place >= ($search_str_length)) {
+            $match = true;
+        }
+        if ($count > 20) {
+            break;
+        }
+    } while ($complete == false && $match == false);
     if ($match == false) {
         echo '未匹配';
     } else {
-        echo '匹配位置起' . $search_str_length . '起';
+        echo '匹配检测字符串位置第' . ($str_place - $search_str_place) . '起';
     }
 }
+echo kmpCheck($argv[1], $argv[2]);
